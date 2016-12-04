4cb45489eb5de6e295c0f8e56459f55ab41b1934
MF1996 <makam-elchahid@outlook.fr>
2016-12-04 16:31:41 +0100

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,27 +2,27 @@
  * Created by Fatmi on 02/12/2016.
  */
 public class Rectangle implements Forme {
-    private double L;
-    private double l;
-    public Rectangle(double L , double l){
-        this.L=L;
-        this.l=l;
+    private double longeur;
+    private double largeur;
+    public Rectangle(double largeur , double longeur){
+        this.largeur=largeur;
+        this.longeur=longeur;
     }
 
-    public double getL(){
-        return L;
+    public double getLongeur(){
+        return longeur;
     }
-    public double getl(){
-        return l;
+    public double getLargeur(){
+        return largeur;
     }
     public double surface(){
-        return L*l;
+        return longeur*largeur;
     }
     public double perimetre(){
-        return(L+l)*2;
+        return(lobgeur+largeur)*2;
     }
     public String toString(){
-        return "la largeur est " + L + "la longeur est " + l + "la surface est " + surface() + "le perimetre est " + perimetre();
+        return "la largeur est " + largeur + "la longeur est " + longeur + "la surface est " + surface() + "le perimetre est " + perimetre();
     }
     public Object  clone(){
         return null ;
